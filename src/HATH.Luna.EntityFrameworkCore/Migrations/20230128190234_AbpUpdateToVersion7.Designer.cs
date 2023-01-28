f99436dd0aac824ac2c5dabc44bbbf0b9023f54d
Atakan <atakangocer98@gmail.com>
2023-01-28 22:25:31 +0300

    LAB-3 : ABP version 7.0 upgrade and test configuration

--- src/HATH.Luna.EntityFrameworkCore/Migrations/20230128190234_AbpUpdateToVersion7.Designer.cs
+++ src/HATH.Luna.EntityFrameworkCore/Migrations/20230128190234_AbpUpdateToVersion7.Designer.cs
@@ -4,6 +4,7 @@ using HATH.Luna.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace HATH.Luna.Migrations
 {
     [DbContext(typeof(LunaDbContext))]
-    partial class LunaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230128190234_AbpUpdateToVersion7")]
+    partial class AbpUpdateToVersion7
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
